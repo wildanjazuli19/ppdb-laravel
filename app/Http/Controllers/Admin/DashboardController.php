48d6a27f3679aea9b9385a29df801ef168529b91
Wildan Jazuli <wildan@airs-MacBook-Air.local>
2026-06-16 19:16:48 +0700

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 
 class DashboardController extends Controller
 {
+    // public function index()
+    // {
+    //     return view('admin.dashboard');
+    // }
+
     public function index()
     {
-        return view('admin.dashboard');
+        $data = [
+            'totalStudents' => Student::count(),
+            'totalVerified' => Student::where('status_verifikasi', 'diterima')->count(),
+            'totalAccepted' => Student::where('status', 'accepted')->count(),
+            'totalRejected' => Student::where('status', 'rejected')->count(),
+        ];
+
+        return view('admin.dashboard', $data);
     }
 }
