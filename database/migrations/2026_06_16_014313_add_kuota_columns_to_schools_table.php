9bf26b7876a7d781c8bf8ac94e289e352e4c14bc
Wildan Jazuli <wildan@airs-MacBook-Air.local>
2026-06-16 08:48:49 +0700

    feat:add kuota column

---
+++ database/migrations/2026_06_16_014313_add_kuota_columns_to_schools_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->integer('kuota_prestasi')->default(0);
+            $table->integer('kuota_zonasi')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->dropColumn([
+                'kuota_prestasi',
+                'kuota_zonasi',
+            ]);
+        });
+    }
+};
