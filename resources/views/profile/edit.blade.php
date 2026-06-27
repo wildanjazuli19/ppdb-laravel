dc12a59a36d144471767104062c280f429a2a651
Wildan Jazuli <wildan@airs-MacBook-Air.local>
2026-06-27 16:25:18 +0700

    fix profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,33 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+@section('content')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto space-y-6">
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+        <div class="bg-white dark:bg-gray-800 rounded-2xl shadow p-6">
+
+            <h1 class="text-2xl font-bold mb-6">
+                Profile
+            </h1>
+
+            @include('profile.partials.update-profile-information-form')
+
         </div>
+
+        <div class="bg-white dark:bg-gray-800 rounded-2xl shadow p-6">
+
+            @include('profile.partials.update-password-form')
+
+        </div>
+
+        <div class="bg-white dark:bg-gray-800 rounded-2xl shadow p-6">
+
+            @include('profile.partials.delete-user-form')
+
+        </div>
+
     </div>
-</x-app-layout>
+</div>
+
+@endsection
